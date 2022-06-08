36bf002e3d9fc82da3ca6785f4341083fbd7759c
nhannn87dn <nhannn87dn@gmail.com>
2022-06-08 16:43:16 +0700

    Updates

---
+++ media/index.js
@@ -0,0 +1,32 @@
+
+export default  audios = [
+  {
+    src: AnhSaoVaBauTroi,
+    title: "Ánh sao và bầu trời",
+    artist: "T.R.I",
+    time: '04:21',
+    cover: AnhSaoVaBauTroiCover
+  },
+  {
+    src: DeVuong,
+    title: "Đế Vương",
+    artist: "Đình Dũng, ACV",
+    time: '04:22',
+    cover: DeVuongCover
+  },
+  {
+    src: TungThuong,
+    title: "Từng Thương",
+    artist: "Phan Duy Anh, ACV",
+    time: '04:10',
+    cover: TungThuongCover
+  },
+  {
+    src: YeuEmHonMoiNgay,
+    title: "Yêu Em Hơn Mỗi Ngày",
+    artist: "Andiez",
+    time: '04:16',
+    cover: YeuEmHonMoiNgayCover
+  }
+];
+
